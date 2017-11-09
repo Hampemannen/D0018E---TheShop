6ff1451bd862bcd3353e628657c43508146bfb41
Hampus Holmström <Hampe.holmstrom@gmail.com>
2017-11-09 19:50:50 +0100

    Add files via upload

---
+++ shop/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+ <html>
+
+ <head>
+
+ <title>The Shop</title>
+ <link rel="stylesheet" href="styles/style.css" media="all" />
+
+ </head>
+
+
+ <body>
+
+   <div class="main_wrapper">
+
+    <div class="header_wrapper">
+      <img src="images/shoplogo.jpg"
+    </div>
+
+    <div class="menubar"></div>
+
+    <div class="content_wrapper">
+
+        <div id="sidebar">Sidebar</div>
+        <div id="conten_area">Content</div>
+        <div id="footer">Footer</div>
+    </div>
+
+
+   </div>
+
+
+<!-- <?php
+
+echo "Hello"
+
+?> -->
+
+
+ </body>
+ </html>
